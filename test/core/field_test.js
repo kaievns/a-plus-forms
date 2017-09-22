dcf77741ac5a2067fe4f9fde82bebd442b21fb61
Nikolay Nemshilov <nemshilov@gmail.com>
2017-09-22 14:03:09 +1000

    Moving the compound input tests back in time

--- test/core/field_test.js
+++ test/core/field_test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { spy } from 'sinon';
 import { mount } from 'enzyme';
-import { field } from '../../src';
+import { field, TextInput, PasswordInput } from '../../src';
 
 @field()
 class Input extends React.Component {
@@ -82,6 +82,50 @@ describe.only('field', () => {
       expect(field.value).to.eql('Nikolay');
       expect(onChange).to.have.been.calledWith('Nikolay');
       expect(render.html()).to.eql('<input value="Nikolay">');
+    });
+  });
+
+  describe('compound fields', () => {
+    @field()
+    class CompoundInput extends React.Component {
+      render() {
+        return (
+          <div>
+            <TextInput name="username" />
+            <PasswordInput name="password" />
+          </div>
+        );
+      }
+    }
+
+    const render = mount(<CompoundInput />);
+    const [input] = render.nodes;
+
+    it('has empty values by default', () => {
+      expect(input.value).to.eql({
+        username: undefined,
+        password: undefined
+      });
+    });
+
+    it('picks up changes in the sub-fields', () => {
+      render.find('input[name="username"]').simulate('change', {
+        target: { value: 'nikolay' }
+      });
+
+      expect(input.value).to.eql({
+        username: 'nikolay',
+        password: undefined
+      });
+
+      render.find('input[name="password"]').simulate('change', {
+        target: { value: 'Ba(k0n!' }
+      });
+
+      expect(input.value).to.eql({
+        username: 'nikolay',
+        password: 'Ba(k0n!'
+      });
     });
   });
 });
