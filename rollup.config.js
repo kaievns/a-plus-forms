4d1041dc4ca46d573efa86466a25326cf9dbf826
Nikolay Nemshilov <nemshilov@gmail.com>
2017-11-30 11:22:02 +1100

    Adding rollup for production bundling

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import babel from 'rollup-plugin-babel';
+
+const babelrc = JSON.parse(fs.readFileSync('./.babelrc'));
+const { env: { production: babelConfig } } = babelrc;
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/index.js',
+    exports: 'named',
+    format: 'cjs'
+  },
+  external: ['react', 'prop-types'],
+  plugins: [
+    babel(
+      Object.assign(babelConfig, {
+        babelrc: false
+      })
+    )
+  ]
+};
