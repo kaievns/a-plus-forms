493226c7f1e55a0f7ddc85d195514a7b185616fd
Nikolay Nemshilov <nemshilov@gmail.com>
2018-04-19 11:15:52 +1000

    Fixing rollup for the new dep

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ export default {
     exports: 'named',
     format: 'cjs'
   },
-  external: ['react', 'prop-types'],
+  external: ['react', 'prop-types', 'fast-deep-equal'],
   plugins: [
     babel(
       Object.assign(babelConfig, {
